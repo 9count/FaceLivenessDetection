243a0a237d2be1ad3f10a1a2117bc7cffffe5976
Jason Chung <qazwsx5219430630@gmail.com>
2024-05-30 15:42:03 +0800

    chore: Import DepthToJET helper class from apple's sample code

---
+++ Sources/FaceLivenessDetection/UI/FaceDetectionView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 鍾哲玄 on 2024/5/30.
+//
+
+import SwiftUI
+
+public struct FaceDetectionView: View {
+    public init() {}
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceDetectionView()
+}
