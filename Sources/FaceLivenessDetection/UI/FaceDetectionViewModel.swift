1b69516b49276587afc8c72cf53f0b3ad8dcd4da
ReadMyName <qazwsx5219430630@gmail.com>
2024-07-16 16:55:25 +0800

    feat: Add documentation for FaceLivenessView usage

--- Sources/FaceLivenessDetection/UI/FaceDetectionViewModel.swift
+++ Sources/FaceLivenessDetection/UI/FaceDetectionViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import Combine
 
 public final class FaceDetectionViewModel: ObservableObject {
+    /// Current state of face detection, indicating if a face is present or not.
     @Published public var instruction: FaceDetectionState = .noFace {
         didSet {
             if instruction == .faceFit {
@@ -18,16 +19,29 @@ public final class FaceDetectionViewModel: ObservableObject {
         }
     }
 
-    @Published var predictionResult: LivenessDataModel?
+    /// Stores the prediction results from the liveness detection model.
+    @Published public var predictionResult: LivenessDataModel?
+    /// Controls the visibility of the camera preview layer. (For Debug)
     @Published var hidePreviewLayer = false
-    @Published var lowLightEnvironment = false
-    @Published var canAnalyzeFace = false
-    @Published var showProgress = false
 
-    let captureImagePublisher = PassthroughSubject<Void, Never>()
-    let resumeSessionPublisher = PassthroughSubject<Void, Never>()
+    /// Indicates if the environment has low light conditions.
+    @Published var lowLightEnvironment = false
+    /// Flag to determine if the face can be analyzed.
+    @Published public var canAnalyzeFace = false
+
+    /// Indicates whether the app is currently showing verifying progress.
+    @Published public var showProgress = false
+
+    /// Publisher to trigger an image capture.
+    public let captureImagePublisher = PassthroughSubject<Void, Never>()
+
+    /// Publisher to signal a resume in the capture session.
+    public let resumeSessionPublisher = PassthroughSubject<Void, Never>()
+
+    /// Set of AnyCancellable for storing subscriptions.
     private var cancellables = Set<AnyCancellable>()
 
+    /// Resets the view model to its initial state.
     public func reset() {
         instruction = .noFace
         predictionResult = nil
@@ -35,6 +49,7 @@ public final class FaceDetectionViewModel: ObservableObject {
         showProgress = false
     }
 
+    /// Sets up a delay timer after which the face can be analyzed.
     public func setupDelayTimer() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             self.canAnalyzeFace = true
