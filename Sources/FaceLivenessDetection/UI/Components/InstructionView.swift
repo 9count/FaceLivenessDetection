d239f514ec02ea16d5918735ffab65bd0dca2b26
Tate <wind15973@gmail.com>
2025-06-17 18:02:50 +0800

    feat: update font (#23)

--- Sources/FaceLivenessDetection/UI/Components/InstructionView.swift
+++ Sources/FaceLivenessDetection/UI/Components/InstructionView.swift
@@ -17,7 +17,7 @@ public struct InstructionView: View {
     public var body: some View {
         Text(instruction.rawValue)
             .foregroundStyle(instruction == .faceFit ? Color(.greenExtraDark) : Color(.redDark))
-            .font(UIFont(name: "ABC-Regular", size: 16))
+            .font(.custom("ABCSolar-Regular", size: 16))
             .multilineTextAlignment(.center)
             .frame(maxWidth: .infinity)
     }
